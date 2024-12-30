640d56520176c0324cbe07bb96673e461c271c70
MacarenaQuijadaG <mcarena.quijada@outlook.com>
2024-12-29 22:51:39 -0300

    Se añaden cambios en el codigo que significaron cambios en su totalidad añadienso javascript como tambien tipos de letra

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("h1");
+    title.addEventListener("mouseover", () => {
+      title.style.color = "#747975"; 
+    });
+    title.addEventListener("mouseout", () => {
+      title.style.color = "#0a0a0a"; 
+    });
+
+    const navLinks = document.querySelectorAll(".nav-link");
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navLinks.forEach(nav => nav.classList.remove("active"));
+        link.classList.add("active");
+      });
+    });
+  });
